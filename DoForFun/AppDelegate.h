b8a29a258c8a027fc47237003de0306680307881
GuGuiJun <ggj@centit.com>
2017-01-05 15:52:45 +0800

    upload code

---
+++ DoForFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoForFun
+//
+//  Created by Gu GuiJun on 2017/1/5.
+//  Copyright © 2017年 Gu GuiJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoForFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoForFun
+//
+//  Created by Gu GuiJun on 2017/1/5.
+//  Copyright © 2017年 Gu GuiJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
